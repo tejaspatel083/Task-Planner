09967a60005f5428513cca15809adf867fbe9b2f
$a! <kalli.saikiran@outlook.com>
2020-10-24 16:32:14 -0400

    Create Account Page created

---
+++ app/src/main/java/com/example/taskplanner/CreateAccountPage.java
@@ -0,0 +1,14 @@
+package com.example.taskplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateAccountPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account_page);
+    }
+}
