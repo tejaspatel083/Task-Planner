a212271b226ffcd11df070449874a24c671c25dc
TejasPatel <tjp083@gmail.com>
2020-10-25 16:42:49 -0400

    My Task List Page created

---
+++ app/src/main/java/com/example/taskplanner/MyTaskListPage.java
@@ -0,0 +1,14 @@
+package com.example.taskplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyTaskListPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_task_list_page);
+    }
+}
