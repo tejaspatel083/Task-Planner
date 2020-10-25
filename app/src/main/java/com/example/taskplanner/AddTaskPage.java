87e19c61da8fbf3e0f20390b36f5077a54863712
sahil <sahil33rana@gmail.com>
2020-10-25 15:40:16 -0400

    task page created

---
+++ app/src/main/java/com/example/taskplanner/AddTaskPage.java
@@ -0,0 +1,14 @@
+package com.example.taskplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddTaskPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task_page);
+    }
+}
