5837f91088d5df5a7b00d2ee5113f095084bfcfa
Manali <thakkarmanali2015@gmail.com>
2020-11-01 19:08:49 -0500

    Firebase Authentication linked with create Account

--- app/src/main/java/com/example/taskplanner/CreateAccountPage.java
+++ app/src/main/java/com/example/taskplanner/CreateAccountPage.java
@@ -1,5 +1,6 @@
 package com.example.taskplanner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -8,13 +9,23 @@ import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class CreateAccountPage extends AppCompatActivity {
 
-    private EditText user_pwd1,user_pwd2;
+    private EditText user_pwd1,user_pwd2,user_name,user_email;
     private TextView v1,v2,iv1,iv2;
+    private Button Createbtn;
+    private FirebaseAuth firebaseAuth;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -22,12 +33,19 @@ public class CreateAccountPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account_page);
 
+        user_email = findViewById(R.id.CreateEmail);
+        user_name = findViewById(R.id.CreateName);
         user_pwd1 = findViewById(R.id.CreatePassword);
         user_pwd2 = findViewById(R.id.CreateRePassword);
+        Createbtn = findViewById(R.id.CreateBtn);
+
         v1 = findViewById(R.id.visible1);
         iv1 = findViewById(R.id.notvisible1);
         v2 = findViewById(R.id.visible2);
         iv2 = findViewById(R.id.notvisible2);
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         user_pwd1.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -75,5 +93,73 @@ public class CreateAccountPage extends AppCompatActivity {
                 v2.setVisibility(View.VISIBLE);
             }
         });
+
+
+
+        Createbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email=user_email.getText().toString().trim();
+                String password = user_pwd1.getText().toString().trim();
+
+
+               firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                   @Override
+                   public void onComplete(@NonNull Task<AuthResult> task) {
+
+                       if(task.isSuccessful())
+                       {
+                           sendEmailVerification();
+
+                       }
+
+                       else
+                       {
+
+                       }
+                   }
+
+
+               });
+            }
+        });
+
     }
+
+    private void sendEmailVerification() {
+
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if(firebaseUser != null)
+        {
+            firebaseUser.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if(task.isSuccessful())
+                    {
+                        firebaseAuth.signOut();
+                        Toast.makeText(CreateAccountPage.this, "Registration is complete", Toast.LENGTH_SHORT).show();
+                    }
+
+                    else
+                    {
+                        Toast.makeText(CreateAccountPage.this, "Verification is not complete", Toast.LENGTH_SHORT).show();
+
+                    }
+
+
+
+
+                }
+            });
+
+        }
+
+
+
+
+
+    }
+
+
 }
